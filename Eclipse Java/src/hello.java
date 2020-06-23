f8d248eecd43f1d90b8490f9d447ed8497ddc1a1
Su-Kyung <45833635+Su-Kyung@users.noreply.github.com>
2020-06-23 20:19:56 +0900

    이클립스 환경설정(Java)
    - 문자열 출력해보기
    
    너무 오랜만,,,,, 다 까먹었다

---
+++ Eclipse Java/src/hello.java
@@ -0,0 +1,10 @@
+
+public class hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello!");
+		System.out.println("자바 기억 더듬기");
+	}
+
+}
