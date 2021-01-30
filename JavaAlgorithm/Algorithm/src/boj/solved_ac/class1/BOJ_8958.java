26202742373131744b70fe709d270794aed37bd8
Su-Kyung <45833635+Su-Kyung@users.noreply.github.com>
2021-01-30 22:21:04 +0900

    [Java] BOJ 8958: 음계
    이거 못풀었었는데,,, 오늘은 풀었다,,,,,
    nextInt() 하고 그 다음 nextLine()하면 두번째 줄을 바로 읽는것이 아니라 첫번째 줄을 (읽은 다음부터인지는 모르겠지만) 읽는다.
    그래서 미리 nextLine() 한 번 해줘야함

---
+++ JavaAlgorithm/Algorithm/src/boj/solved_ac/class1/BOJ_8958.java
@@ -0,0 +1,37 @@
+package boj.solved_ac.class1;
+// OX퀴즈(분류: 구현,문자열)
+// solved
+import java.util.Scanner;
+
+public class BOJ_8958 {
+	static int test_case;
+	static char[] inputs;
+	static int count;
+	static int score;
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		test_case = sc.nextInt();
+		inputs=sc.nextLine().toCharArray();
+		
+		for (int t = 0; t < test_case; t++) {
+			inputs = sc.nextLine().toCharArray();
+			count = 0;
+			score = 0;
+			for (int i = 0; i < inputs.length; i++) {
+				if (inputs[i]=='O') {
+					count++;
+				} else if (inputs[i]=='X'){
+					score += count*(count+1)/2;
+					count = 0;
+				}
+				if (i==inputs.length-1) {
+					score += count*(count+1)/2;
+					count = 0;
+				}
+			}
+			System.out.println(score);
+		}
+		sc.close();
+	}
+}
+	
