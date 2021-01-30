53843d2cd58cff782a79192d541b0df52f16fbcb
Su-Kyung <45833635+Su-Kyung@users.noreply.github.com>
2021-01-31 01:17:57 +0900

    [Java] BOJ 11720: 숫자의 합
    한글자씩 자르기 -> split 또는 charAt
    String을 Int로 -> Integer.parstInt(변환할 String)

---
+++ JavaAlgorithm/Algorithm/src/boj/solved_ac/class1/BOJ_11720.java
@@ -0,0 +1,21 @@
+package boj.solved_ac.class1;
+// 숫자의 합(분류: 수학,문자열,사칙연산)
+// solved
+
+import java.util.Scanner;
+
+public class BOJ_11720 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int count = sc.nextInt();
+		int sum = 0;
+		String[] number = sc.next().split("");
+				
+		for (int i = 0; i < count; i++) {
+			sum+=Integer.parseInt(number[i]);
+		}
+		System.out.println(sum);
+		sc.close();
+	}
+
+}
