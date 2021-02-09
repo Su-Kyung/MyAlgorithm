709540e98d96a20adcbeff5fac768dddb7148c55
Su-Kyung <45833635+Su-Kyung@users.noreply.github.com>
2021-02-09 14:05:42 +0900

    [Java] BOJ 1158: 요세푸스 문제
    StringBuilder로 출력문자 관리
    뒤에 문제 지울 때 한꺼번에 여러개 쓰는 방법은?
    -> StringBuilder 메서드 정리하기

---
+++ JavaAlgorithm/Algorithm/src/boj/BOJ_S5_1158.java
@@ -0,0 +1,34 @@
+package boj;
+// Silver5 요세푸스 문제(큐)
+// solved
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+public class BOJ_S5_1158 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		Queue<Integer> Q = new LinkedList<Integer>();
+		
+		int N = sc.nextInt();
+		int K = sc.nextInt();
+		
+		for (int i = 0; i < N; i++) Q.add(i+1);
+		
+		StringBuilder sb = new StringBuilder();
+		sb.append("<");
+		while (!Q.isEmpty()) {
+			for (int i = 0; i < K-1; i++) Q.add(Q.poll());
+			sb.append(Q.poll()).append(", ");
+		}
+		sb.deleteCharAt(sb.lastIndexOf(" "));
+		sb.deleteCharAt(sb.lastIndexOf(","));
+		sb.append(">");
+		System.out.println(sb.toString());
+		
+		sc.close();
+	}
+
+}
