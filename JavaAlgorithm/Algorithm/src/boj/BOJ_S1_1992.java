48738b49411ebf00c6153dc649cbf5554f6b6df8
Su-Kyung <45833635+Su-Kyung@users.noreply.github.com>
2021-02-18 04:59:33 +0900

    [Java] BOJ 1992: 쿼드트리
    S1 분할정복,재귀
    
    기저조건 찾으니까 바로 풀려버리네....
    기저조건에 대한 아이디어를 찾는게 어렵게 느껴졌다 ㅜㅜ 그래도 재귀에 아주 조금씩은 익숙해지는 느낌..?

---
+++ JavaAlgorithm/Algorithm/src/boj/BOJ_S1_1992.java
@@ -0,0 +1,58 @@
+package boj;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+//S1 쿼드트리(분할정복, 재귀)
+//solved
+
+public class BOJ_S1_1992 {
+
+	static int[][] video;
+	static StringBuilder sb;
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine().trim());
+		video = new int[N][N];
+//		1. 영상을 입력받아 2차원 배열에 저장		
+		String str;
+		for(int i=0; i<N; i++) {
+			str = br.readLine();
+			for(int j=0; j<N; j++) {
+				video[i][j] = str.charAt(j)-'0';
+			}
+		}
+		
+//		2. 영상 압축(재귀)
+		sb = new StringBuilder();
+		compress(0, 0, N, sb);
+		
+//		3. 출력
+		System.out.print(sb);
+	}
+
+	private static void compress(int startR, int startC, int size, StringBuilder s) {
+		if(check(video, startR, startC, size, 0)) {
+			s.append(0);
+			return;
+		} else if(check(video, startR, startC, size, 1)) {
+			s.append(1);
+			return;
+		}
+		s.append("(");
+		compress(startR, startC, size/2, s);
+		compress(startR, startC+size/2, size/2, s);
+		compress(startR+size/2, startC, size/2, s);
+		compress(startR+size/2, startC+size/2, size/2, s);
+		s.append(")");
+	}
+
+	private static boolean check(int[][] arr, int r, int c, int s, int n) {
+		for(int i=r, end=r+s; i<end; i++) {
+			for(int j=c, end2=c+s; j<end2; j++) {
+				if(arr[i][j] != n) return false;
+			}
+		}
+		return true;
+	}
+}
