2307322f6c4342d8f0fb06f2a3f465bc75f65b8b
Su-Kyung <45833635+Su-Kyung@users.noreply.github.com>
2021-02-25 04:45:15 +0900

    [Java] BOJ 20364: 부동산 다툼
    S2
    사소한 조건들을 자꾸 간과해서 여러번 틀렸다. 꼼꼼하게 하자!
    트리를 위에서부터 탐색하는 방법을 생각하다보니 엄청 복잡하게 생각했었는데, 그냥 숫자들의 관계만 보면 엄청쉽게 풀린다. 그리고 목표하는 지점부터 위로 올라가면서 탐색!

---
+++ JavaAlgorithm/Algorithm/src/boj/BOJ_S2_20364.java
@@ -0,0 +1,32 @@
+package boj;
+//S2 부동산 다툼(트리)
+//solved
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ_S2_20364 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		int N = Integer.parseInt(st.nextToken());	//땅 개수
+		int Q = Integer.parseInt(st.nextToken());	//오리 수
+		
+		boolean[] visited = new boolean[N+1];	//이미 방문했는지 체크
+		int goal;	//오리가 원하는 땅번호
+		int pre;	//검사할 부모노드
+		for(int i=0; i<Q; i++) {
+			goal = Integer.parseInt(br.readLine().trim());
+			int ans=0; pre=goal;
+			while(pre!=0) {	//루트까지 검사
+				if(visited[pre]) ans=pre;
+				pre /= 2;
+			}
+			if(ans==0) visited[goal]=true;
+			
+			System.out.println(ans);
+		}
+	}
+}
