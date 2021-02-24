d40185dfbf30eed5757daa1f7df9f2d886917e8f
Su-Kyung <45833635+Su-Kyung@users.noreply.github.com>
2021-02-25 04:43:38 +0900

    [Java] BOJ 12782: 비트 우정지수
    S5
    엄청 간단한 문제인데 비트라는 말에 쫄았다..바보다,,,,,,,

---
+++ JavaAlgorithm/Algorithm/src/boj/BOJ_S5_12782.java
@@ -0,0 +1,34 @@
+package boj;
+// S5 비트 우정지수(그리디 알고리즘,수학)
+// solved
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ_S5_12782 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int T = Integer.parseInt(br.readLine().trim());	//테스트케이스 수
+		StringTokenizer st;
+		String num1, num2;	//비교할 두 수
+		int diff;	//다른 숫자 개수
+		int one, zero;	//num1 기준
+		for(int t=0; t<T; t++) {
+			st = new StringTokenizer(br.readLine(), " ");
+			num1 = st.nextToken();
+			num2 = st.nextToken();
+			one=0; zero=0;
+			for(int i=0; i<num1.length(); i++) {
+				if(num1.charAt(i)==num2.charAt(i)) continue;
+				if(num1.charAt(i)=='1') one++;
+				else zero++;
+			}
+			
+			System.out.println(Math.max(one, zero));
+		}
+	}
+
+}
