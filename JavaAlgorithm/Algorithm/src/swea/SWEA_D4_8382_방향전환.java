3858be9bec9f66e5aadf77e537f19c7b6f9b958b
Su-Kyung <45833635+Su-Kyung@users.noreply.github.com>
2021-03-15 20:04:18 +0900

    [Java] SWEA 8382: 방향 전환
    D4 그냥 규칙을 찾아 풀면 쉽다.
    BFS,(시뮬)도 가능!!

---
+++ JavaAlgorithm/Algorithm/src/swea/SWEA_D4_8382_방향전환.java
@@ -0,0 +1,42 @@
+package swea;
+//D4 방향 전환
+//solved 규칙이 쉬워서 규칙을 찾아서 풀었다
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class SWEA_D4_8382_방향전환 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int T = Integer.parseInt(br.readLine().trim());
+		int x1, x2, y1, y2;
+		int width, height;
+		
+		StringTokenizer st;
+		StringBuilder sb = new StringBuilder();
+		for(int t=1, tEnd=T+1; t<tEnd; t++) {
+			sb.append("#").append(t).append(" ");
+			
+			st = new StringTokenizer(br.readLine(), " ");
+			x1 = Integer.parseInt(st.nextToken());
+			y1 = Integer.parseInt(st.nextToken());
+			x2 = Integer.parseInt(st.nextToken());
+			y2 = Integer.parseInt(st.nextToken());
+			
+			width=Math.abs(x1-x2);
+			height=Math.abs(y1-y2);
+			
+			if(width>height) {
+				if((width-height)%2==0) sb.append(width*2).append("\n");
+				else sb.append(width*2-1).append("\n");
+			} else {
+				if((width-height)%2==0) sb.append(height*2).append("\n");
+				else sb.append(height*2-1).append("\n");
+			}
+		}
+		System.out.println(sb.toString());
+	}
+}
